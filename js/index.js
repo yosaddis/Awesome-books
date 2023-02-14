deb21d0562b428c1b1ceff980a151c0f5fc500e8
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-02-14 20:35:51 +0100

    Update the add and remove function

--- js/index.js
+++ js/index.js
@@ -7,30 +7,21 @@ let books = [
 ];
 
 const addBook = (title, author) => {
-
   // genertate a random id
   const random = Math.random().toString(36).substring(2, 9);
 
   // add a new book to the books array
-
   books.push({
     id: random,
     title,
     author,
   });
-   listBooks();
-}
+};
 
 // remove a book from the books array
 const removeBook = (id) => {
   books = books.filter((book) => book.id !== String(id));
-   listBooks();
-}
+};
 
-// list all books in the books array
-const listBooks = ()  => {
-  for (let i = 0; i < books.length; i += 1) {
-// HTML implementation here
-  }
-}
-
+addBook('Test Title', 'Author'); // add a book
+removeBook(books[0].id); // remove a book
