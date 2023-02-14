ad43ff3c5bb74253a684135a076198850de866f7
yosaddis <60548252+yosaddis@users.noreply.github.com>
2023-02-14 18:47:17 +0300

    Modify index.js for the first four tasks

--- js/index.js
+++ js/index.js
@@ -1,29 +1,42 @@
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
+let books = [
+	{
+		id: Math.random().toString(36).substring(2,9),
+		title:'Test Title',
+		author:'Author',
+	},
+];
+
+
+
+function addBook(title,author){
+	let random = Math.random().toString(36).substring(2,9);
+	books.push({
+			id: random,
+			title,
+			author, 
+	});
+	listBooks();
 }
 
-const bookTitle = document.querySelector('.title');
-const bookAuthor = document.querySelector('.author');
-const addNewBookForm = document.querySelector('.bookForm');
-const {title} = addNewBookForm.elements;
-const {author} = addNewBookForm.elements;  
-
-let books = {};
-
-console.log(books);
-
-function addBook(){
-		let newBook = new Book(title,author);
-		books.push(newBook);
-	}
-
-function removeBook(index){
-	books.splice(index,1);
+function removeBook(id) {
+	books = books.filter((book) => book.id !== String(id));
+	listBooks();
 }
 
-addNewBookForm.addEventListener('submit', () => {
+function listBooks() {
+	for (const item of books) {
+		console.log(item);
+	}	
+}
 
-});
+
+listBooks();
+console.log("===========================");
+addBook('Test2','Many author');
+addBook('Test3', 'Many author');
+console.log("===========================");
+listBooks();
+console.log("===========================");
+removeBook(books[0].id);
+listBooks();
+console.log("===========================");
