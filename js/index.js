df865650e37bc9b6786f2a8fcac3997845c911aa
yosaddis <60548252+yosaddis@users.noreply.github.com>
2023-02-14 19:11:17 +0300

    Modify index.js for linter errors

--- js/index.js
+++ js/index.js
@@ -1,42 +1,39 @@
 let books = [
-	{
-		id: Math.random().toString(36).substring(2,9),
-		title:'Test Title',
-		author:'Author',
-	},
+  {
+    id: Math.random().toString(36).substring(2, 9),
+    title: 'Test Title',
+    author: 'Author',
+  },
 ];
 
-
-
-function addBook(title,author){
-	let random = Math.random().toString(36).substring(2,9);
-	books.push({
-			id: random,
-			title,
-			author, 
-	});
-	//listBooks();
+function addBook(title, author) {
+  const random = Math.random().toString(36).substring(2, 9);
+  books.push({
+    id: random,
+    title,
+    author,
+  });
+  // listBooks();
 }
 
 function removeBook(id) {
-	books = books.filter((book) => book.id !== String(id));
-	//listBooks();
+  books = books.filter((book) => book.id !== String(id));
+  // listBooks();
 }
 
 function listBooks() {
-	for (const item of books) {
-		console.log(item);
-	}	
+  for (let i = 0; i < books.length; i += 1) {
+    console.log(books[i]);
+  }
 }
 
-
 listBooks();
-console.log("===========================");
-addBook('Test2','Many author');
+console.log('===========================');
+addBook('Test2', 'Many author');
 addBook('Test3', 'Many author');
-console.log("===========================");
+console.log('===========================');
 listBooks();
-console.log("===========================");
+console.log('===========================');
 removeBook(books[0].id);
 listBooks();
-console.log("===========================");
+console.log('===========================');
