3fd1b6357636f3b50c6de546833c86468768ab29
yosaddis <60548252+yosaddis@users.noreply.github.com>
2023-02-16 00:54:46 +0300

    Modify style.css and index.js for linter errors

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
 // render books
 const renderBooks = () => {
-	// Get DOM elements'
-	const bookList = document.querySelector('.books__list');
+  // Get DOM elements'
+  const bookList = document.querySelector('.books__list');
 
-	// get the books from the books array from localStorage
-	const booksCollection = localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
-	// check the length of the books array
+  // get the books from the books array from localStorage
+  const booksCollection = localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
+  // check the length of the books array
 
-	if (booksCollection.length === 0) {
-		// clear the list
-		bookList.innerHTML = '';
+  if (booksCollection.length === 0) {
+    // clear the list
+    bookList.innerHTML = '';
 
-		// update the DOM with no books message
-		bookList.innerHTML = '<p class="books__list-item">No books to show</p>';
-	} else {
-		// loop through the books array
-		const bookshtml = booksCollection.map((book) => `
+    // update the DOM with no books message
+    bookList.innerHTML = '<p class="books__list-item">No books to show</p>';
+  } else {
+    // loop through the books array
+    const bookshtml = booksCollection.map((book) => `
       <div class="books__list-item">
         <p class="books__list-item-title">${book.title}</p>
         <p class="books__list-item-author">${book.author}</p>
@@ -23,74 +23,73 @@ const renderBooks = () => {
       </div>
     `).join('');
 
-		// clear the list
-		bookList.innerHTML = '';
+    // clear the list
+    bookList.innerHTML = '';
 
-		// update the DOM with the new list
-		bookList.insertAdjacentHTML('afterbegin', bookshtml);
-	}
+    // update the DOM with the new list
+    bookList.insertAdjacentHTML('afterbegin', bookshtml);
+  }
 };
 
 // add a book function
 const addBook = (book) => {
-	// get the books from the books array from localStorage
-	const booksCollection = localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
-	booksCollection.push(book);
+  // get the books from the books array from localStorage
+  const booksCollection = localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
+  booksCollection.push(book);
 
-	// update the books array in localStorage
-	localStorage.setItem('books', JSON.stringify(booksCollection));
+  // update the books array in localStorage
+  localStorage.setItem('books', JSON.stringify(booksCollection));
 
-	// call the renderBooks function
-	renderBooks();
+  // call the renderBooks function
+  renderBooks();
 };
 
 // remove a book function
 const removeBook = (id) => {
-	// get the books from the books array from localStorage
-	const booksCollection = JSON.parse(localStorage.getItem('books'));
-	const updatedBooksCollection = booksCollection.filter((book) => book.id !== id);
+  // get the books from the books array from localStorage
+  const booksCollection = JSON.parse(localStorage.getItem('books'));
+  const updatedBooksCollection = booksCollection.filter((book) => book.id !== id);
 
-	// update the books array in localStorage
-	localStorage.setItem('books', JSON.stringify(updatedBooksCollection));
+  // update the books array in localStorage
+  localStorage.setItem('books', JSON.stringify(updatedBooksCollection));
 
-	// call the renderBooks function
-	renderBooks();
+  // call the renderBooks function
+  renderBooks();
 };
-
 
 // render  books on the page when page loads
 window.addEventListener('DOMContentLoaded', () => {
-	// call the renderBooks function when the page loads
-	renderBooks();
+  // call the renderBooks function when the page loads
+  renderBooks();
 });
 
 // add event listener to the page
 document.addEventListener('click', (e) => {
-	if (e.target.dataset.input === 'submit') {
-		// Get DOM elements
-		const form = document.querySelector('.books__addbook-form');
+  if (e.target.dataset.input === 'submit') {
+    // Get DOM elements
+    const form = document.querySelector('.books__addbook-form');
 
-		// check form validity
-		if (form.checkValidity()) {
-			// prevent the default behaviour
-			e.preventDefault();
+    // check form validity
+    if (form.checkValidity()) {
+      // prevent the default behaviour
+      e.preventDefault();
 
-			// get the values from the form
-			const formData = new FormData(form);
-			const title = formData.get('title');
-			const author = formData.get('author');
-			const id = Math.random().toString(36).substring(2, 9);
+      // get the values from the form
+      const formData = new FormData(form);
+      const title = formData.get('title');
+      const author = formData.get('author');
+      const id = Math.random().toString(36).substring(2, 9);
 
-			const newBook = { id, title, author };
+      const newBook = { id, title, author };
 
-			// call the addBook function
-			addBook(newBook);
+      // call the addBook function
+      addBook(newBook);
 
-			// clear form
-			form.reset();
-		}
-	} else if (e.target.dataset.id) {
-		// call the removeBook function
-		removeBook(e.target.dataset.id);
-	}
+      // clear form
+      form.reset();
+    }
+  } else if (e.target.dataset.id) {
+    // call the removeBook function
+    removeBook(e.target.dataset.id);
+  }
 });
