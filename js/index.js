ae6f3b6218aceebdcd62944d32be86f1e61e3dd6
yosaddis <60548252+yosaddis@users.noreply.github.com>
2023-02-16 23:47:26 +0300

    Modify index.js for class implementation

--- js/index.js
+++ js/index.js
@@ -1,40 +1,33 @@
-// creates a class for book
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
-}
-
 // create a class for book collection
-class bookList {
-  constructor() {
-    if (localStorage.getItem('books')) {
-      this.list = [...JSON.parse(localStorage.getItem('bookList'))];
-    } else {
-      this.list = [];
-    }
-  }
-}
+class Books {
+	constructor() {
+		this.list;
+	}
 
-// render books
-const renderBooks = () => {
-  // Get DOM elements'
-  const bookList = document.querySelector('.books__list');
+	// get books from local storage
+	get list() {
+		return localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
+	}
 
-  // get the books from the books array from localStorage
-  const booksCollection = localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
-  // check the length of the books array
+	// render books
+	renderBooks() {
+		// Get DOM elements'
+		const bookList = document.querySelector('.books__list');
 
-  if (booksCollection.length === 0) {
-    // clear the list
-    bookList.innerHTML = '';
+		// get the books from the books array from localStorage
+		const booksCollection = this.list;
 
-    // update the DOM with no books message
-    bookList.innerHTML = '<p class="books__list-item">No books to show</p>';
-  } else {
-    // loop through the books array
-    const bookshtml = booksCollection.map((book) => `
+		// check the length of the books array
+		if (booksCollection.length === 0) {
+			// clear the list
+			bookList.innerHTML = '';
+
+			// update the DOM with no books message
+			bookList.innerHTML = '<p class="books__list-item">No books to show</p>';
+
+		} else {
+			// loop through the books array
+			const bookshtml = booksCollection.map((book) => `
       <div class="books__list-item">
         <p class="books__list-item-title">${book.title}</p>
         <p class="books__list-item-author">${book.author}</p>
@@ -42,73 +35,85 @@ const renderBooks = () => {
       </div>
     `).join('');
 
-    // clear the list
-    bookList.innerHTML = '';
+			// clear the list
+			bookList.innerHTML = '';
 
-    // update the DOM with the new list
-    bookList.insertAdjacentHTML('afterbegin', bookshtml);
-  }
+			// update the DOM with the new list
+			bookList.insertAdjacentHTML('afterbegin', bookshtml);
+		}
+	}
+
+	// add a book function
+	addBook(book) {
+		// get the books from the books array from localStorage
+		const booksCollection = localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
+		booksCollection.push(book);
+
+		// update the books array in localStorage
+		localStorage.setItem('books', JSON.stringify(booksCollection));
+
+		// call the renderBooks function
+		this.renderBooks();
+	}
+
+	// remove a book function
+	removeBook(id) {
+		// get the books from the books array from localStorage
+		const booksCollection = JSON.parse(localStorage.getItem('books'));
+		const updatedBooksCollection = booksCollection.filter((book) => book.id !== id);
+
+		// update the books array in localStorage
+		localStorage.setItem('books', JSON.stringify(updatedBooksCollection));
+
+		// call the renderBooks function
+		this.renderBooks();
+	}
+
 };
 
-// add a book function
-const addBook = (book) => {
-  // get the books from the books array from localStorage
-  const booksCollection = localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
-  booksCollection.push(book);
-
-  // update the books array in localStorage
-  localStorage.setItem('books', JSON.stringify(booksCollection));
-
-  // call the renderBooks function
-  renderBooks();
-};
-
-// remove a book function
-const removeBook = (id) => {
-  // get the books from the books array from localStorage
-  const booksCollection = JSON.parse(localStorage.getItem('books'));
-  const updatedBooksCollection = booksCollection.filter((book) => book.id !== id);
-
-  // update the books array in localStorage
-  localStorage.setItem('books', JSON.stringify(updatedBooksCollection));
-
-  // call the renderBooks function
-  renderBooks();
-};
 
 // render  books on the page when page loads
 window.addEventListener('DOMContentLoaded', () => {
-  // call the renderBooks function when the page loads
-  renderBooks();
+	// create a new instance of the Books class
+	const books = new Books();
+
+	// call the renderBooks method from books class when the page loads
+	books.renderBooks();
 });
 
 // add event listener to the page
 document.addEventListener('click', (e) => {
-  if (e.target.dataset.input === 'submit') {
-    // Get DOM elements
-    const form = document.querySelector('.books__addbook-form');
+	if (e.target.dataset.input === 'submit') {
+		// Get DOM elements
+		const form = document.querySelector('.books__addbook-form');
 
-    // check form validity
-    if (form.checkValidity()) {
-      // prevent the default behaviour
-      e.preventDefault();
+		// check form validity
+		if (form.checkValidity()) {
+			// prevent the default behaviour
+			e.preventDefault();
 
-      // get the values from the form
-      const formData = new FormData(form);
-      const title = formData.get('title');
-      const author = formData.get('author');
-      const id = Math.random().toString(36).substring(2, 9);
+			// get the values from the form
+			const formData = new FormData(form);
+			const title = formData.get('title');
+			const author = formData.get('author');
+			const id = Math.random().toString(36).substring(2, 9);
 
-      const newBook = { id, title, author };
+			const newBook = { id, title, author };
 
-      // call the addBook function
-      addBook(newBook);
+			// create a new instance of the Books class
+			const books = new Books();
 
-      // clear form
-      form.reset();
-    }
-  } else if (e.target.dataset.id) {
-    // call the removeBook function
-    removeBook(e.target.dataset.id);
-  }
+			// call the addBook function
+			books.addBook(newBook);
+
+			// clear form
+			form.reset();
+		}
+	} else if (e.target.dataset.id) {
+		// create a new instance of the Books class
+		const books = new Books();
+
+		// call the removeBook function
+		books.removeBook(e.target.dataset.id);
+	}
 });
