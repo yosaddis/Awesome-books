ce30f32e9594b1ff81bb10e913da6cf4d8318524
yosaddis <60548252+yosaddis@users.noreply.github.com>
2023-02-18 12:45:05 +0300

    Modify index.js to add current datetime function

--- js/index.js
+++ js/index.js
@@ -1,6 +1,43 @@
 // Get DOM elements
 const dateDisplay = document.querySelector('.header__date');
 
+// create a date-time function
+const CurrentDateTime = () => {
+	const dateObject = new Date();
+	const yearNow = dateObject.getFullYear();
+	const monthNow = dateObject.toLocaleString('default', { month: 'long' });
+	const dayNow = dateObject.getDate();
+	const timeNow = dateObject.toLocaleTimeString();
+
+	// Append suffix to day
+	if (dayNow > 3 && dayNow < 21) {
+		const date = `${monthNow} ${dayNow}th ${yearNow}`;
+		dateDisplay.innerHTML = `${date}, ${timeNow}`;
+	} else {
+		const d = dayNow % 10;
+		let date = '';
+		switch (d) {
+			case 1:
+				date = `${monthNow} ${dayNow}st ${yearNow}`;
+				dateDisplay.innerHTML = `${date}, ${timeNow}`;
+				break;
+			case 2:
+				date = `${monthNow} ${dayNow}nd ${yearNow}`;
+				dateDisplay.innerHTML = `${date}, ${timeNow}`;
+				break;
+			case 3:
+				date = `${monthNow} ${dayNow}rd ${yearNow}`;
+				dateDisplay.innerHTML = `${date}, ${timeNow}`;
+				break;
+			default:
+				date = `${monthNow} ${dayNow}th ${yearNow}`;
+				dateDisplay.innerHTML = `${date}, ${timeNow}`;
+				break;
+		}
+	}
+};
+
+setInterval(CurrentDateTime, 1000);
 
 const showAndRemoveSuccessMessage = (target) => {
 	const successMessage = document.createElement('p');
