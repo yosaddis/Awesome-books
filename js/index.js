7d685aea560fa419d005c8f87afc07a74241de6f
yosaddis <60548252+yosaddis@users.noreply.github.com>
2023-02-18 12:47:59 +0300

    Modify index.js to add contact page

--- js/index.js
+++ js/index.js
@@ -148,6 +148,28 @@ const displayForm = () => {
 	booksContainer.insertAdjacentHTML('afterbegin', addFormHtml);
 };
 
+// create a contact page display function
+const displayContact = () => {
+	const booksContainer = document.querySelector('.books');
+
+	const contactHtml = `
+  <section class="books__contact">
+    <h2 class="books__contact-title">Contact Information</h2>
+    
+    <div class="books__contact-details">
+      <p>Do you have any question or you just want to say "Hello"? You can reach out to us!</p>
+        <ul class="books__contact-details-info">
+          <li>Our email: mail@mail.com</li>
+          <li>Our phone number: 0043586534422</li>
+          <li>Our address: Streetname 22, 84503 City, Country</li>
+        </ul>
+      </div>
+  </section>
+  `;
+
+	booksContainer.innerHTML = '';
+	booksContainer.insertAdjacentHTML('afterbegin', contactHtml);
+};
 
 // add event listener to the page
 document.addEventListener('click', (e) => {
